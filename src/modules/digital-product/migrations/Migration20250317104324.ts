b461605a52f77f061b416649f5deda0ae21c61d4
pony <10001@qq.com>
2025-03-17 22:10:02 +0800

    feat: handle product deletion step7

--- src/modules/digital-product/migrations/Migration20250317104324.ts
+++ src/modules/digital-product/migrations/Migration20250317104324.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20250313104217 extends Migration {
+export class Migration20250317104324 extends Migration {
 
   override async up(): Promise<void> {
     this.addSql(`create table if not exists "digital_product" ("id" text not null, "name" text not null, "created_at" timestamptz not null default now(), "updated_at" timestamptz not null default now(), "deleted_at" timestamptz null, constraint "digital_product_pkey" primary key ("id"));`);
